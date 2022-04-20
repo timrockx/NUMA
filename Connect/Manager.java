42c51db61729e51e8675c0a663d23d03f0ad9e1a
timrockx <timothyyinlee@gmail.com>
2022-04-20 11:38:37 -0400

    added visitor functionality

--- Connect/Manager.java
+++ Connect/Manager.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class Manager {
     
+    // manger interface
     public static void managerInterface(Connection conn) {
-
         // instantiate scanner
         Scanner in = new Scanner(System.in);
 
@@ -14,13 +14,13 @@ public class Manager {
                 System.out.println("Welcome to the NUMA Property Manager Interface!");
                 
                 int choice;
-
                 do {
                     // interface menu
                     System.out.println("Please select an option below:");
                     System.out.println("\t1: View all property listings.");
                     System.out.println("\t2: View all apartments of a property.");
                     System.out.println("\t3: View all apartments available for rent.");
+                    System.out.println("\t4: Record a visit by a prospective tenant.");
                     System.out.println("\t0: Exit");
                     // switch through choice
                     choice = Integer.parseInt(in.nextLine());
@@ -57,64 +57,93 @@ public class Manager {
                             // display all apartments related to a single property
                             System.out.println("Enter the ID of the property you would like to view.");
                             int pID = Integer.parseInt(in.nextLine());
-                            System.out.println("All apartments at Property " + pID + ": \n");
                             
                             // create query to search for all apartments in given property
                             String aptQuery = "select * from apartment where p_id = ?";
                             PreparedStatement pStmt2 = conn.prepareStatement(aptQuery);
                             pStmt2.setInt(1, pID);
-
                             ResultSet rs2 = pStmt2.executeQuery();
-                            ResultSetMetaData rsmd2 = rs2.getMetaData();
-                            int colNum2 = rsmd2.getColumnCount();
 
-                            // print result set
-                            while(rs2.next()) {
-                                for(int i = 1; i <= colNum2; i++) {
-                                    System.out.print(rs2.getString(i) + " ");
-                                }
+                            // display results
+                            System.out.println("Apartments at Property " + pID + ":");
+                            System.out.println("================");
+                            System.out.println("Apt. Num\tBeds\t\tBaths\t\tSq. Ft.");
+
+                            if(rs2.next() == false) {
+                                System.out.println("[Error]: No apartments found at specified property.");
+                            } else {
+                                // print apartment info
+                                do {
+                                    System.out.println(rs2.getString(1) + "\t\t" + rs2.getString(2) + "\t\t" + rs2.getString(3) + "\t\t" + rs2.getString(4));
+                                } while(rs2.next());
                                 System.out.println();
                             }
-                            System.out.println();
                             break;
                             
                         case 3:
                             // display all apartments available for rent
                             System.out.println("Enter the ID of the property you'd like to view.");
                             pID = Integer.parseInt(in.nextLine());
-                            System.out.println("All apartments available for rent at Property " + pID + ": \n");
 
-                            // create query to search for apartments that have a lease but no tenant (available for rent)
-                            String leaseQuery = "select * from lease where apt_num not in (select apt_num from lives_in)";
+                            // query to search for apartments that have a lease but no tenant (available for rent)
+                            String leaseQuery = "select apt_num, num_beds, num_baths, sq_foot, monthly_price from lease natural join apartment where apt_num not in (select apt_num from lives_in)";
                             PreparedStatement pStmt3 = conn.prepareStatement(leaseQuery);
                             ResultSet rs3 = pStmt3.executeQuery();
 
-                            ResultSetMetaData rsmd3 = rs3.getMetaData();
-                            int colNum3 = rsmd3.getColumnCount();
+                            // display results
+                            System.out.println("Apartments for Rent at Property " + pID + ":");
+                            System.out.println("================");
+                            System.out.println("Apt. Num\tBeds\t\tBaths\t\tSq. Ft.\t\tPrice (per M)");
 
-                            // print result set
-                            while(rs3.next()) {
-                                for(int i = 1; i <= colNum3; i++) {
-                                    System.out.print(rs3.getString(i) + " ");
-                                }
+                            if(rs3.next() == false) {
+                                System.out.println("[Error]: No apartments available for rent at specified property.");
+                            } else {
+                                // print out apartment info
+                                do {
+                                    System.out.println(rs3.getString(1) + "\t\t" + rs3.getString(2) + "\t\t" + rs3.getString(3) + "\t\t" + rs3.getString(4) + "\t\t" + rs3.getString(5));
+                                } while(rs3.next());
                                 System.out.println();
                             }
-                            System.out.println();
+                            break;
+
+                        case 4:
+                            // record a visit by a prospective tenant
+                            System.out.println("Enter the visitor's name: ");
+                            String visitorName = in.nextLine();
+                            System.out.println("Enter the visitor's phone number: ");
+                            String visitorPhone = in.nextLine();
+                            System.out.println("Enter the visitor's email: ");
+                            String visitorEmail = in.nextLine();
+                            // insert query
+                            String visitorQ = "insert into visitor values (?, ?, ?)";
+                            PreparedStatement pStmt4 = conn.prepareStatement(visitorQ);
+                            pStmt4.setString(1, visitorName);
+                            pStmt4.setString(2, visitorPhone);
+                            pStmt4.setString(3, visitorEmail);
+                            // execute query
+                            int rowsChanged = pStmt4.executeUpdate();
+                            if(rowsChanged == 1) {
+                                System.out.println("[Update]: Visitor " + visitorName + " has been added to the database.\n");
+                            } else {
+                                System.out.println("[Error]: Visitor was not added to the database.\n");
+                            }
                             break;
                         
                         default:
+                            // invalid choice
                             System.out.println("Please make a proper selection (1-3).");
                             System.out.println("\t1: View all property listings.");
                             System.out.println("\t2: View all apartments of a property.");
                             System.out.println("\t3: View all available leases of a property.");
+                            System.out.println("\t4: Record a visit by a prospective tenant.");
                             System.out.println("\t0: Exit");
                             break;
                     }
                 } while (choice != 0);
             }
             catch (SQLException sqle) {
-                // sqle.printStackTrace();
                 System.out.println("[Error]: Connect error. Please try again.");
+                sqle.printStackTrace();
             }
         } while (conn == null);
 
